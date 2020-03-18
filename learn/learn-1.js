4690c2b3f2a3d463a3689ada5fe1bf4554cda7a7
Miracool <mklef121@gmail.com>
2020-03-18 21:16:41 +0100

    Started learning react

---
+++ learn/learn-1.js
@@ -0,0 +1,26 @@
+
+/*
+React is a declarative, efficient, and flexible JavaScript library for building 
+user interfaces. It lets you compose complex UIs from small and isolated pieces of 
+code called “components”.
+*/
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+return React.createElement('div', {className: 'shopping-list'},
+  React.createElement('h1', /* ... h1 children ... */),
+  React.createElement('ul', /* ... ul children ... */)
+);
